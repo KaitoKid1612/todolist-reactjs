5eaa0f422c24f447358b528386b37b4890ba99be
Kaito <vielv@waocon-sg.vn>
2024-04-17 14:51:28 +0700

    feat: create signup page

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
-import { Routes } from "react-router-dom"
-import { Route } from "react-router-dom"
-import { BrowserRouter } from "react-router-dom"
-import Login from "./pages/Login"
+import { Routes } from "react-router-dom";
+import { Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
 
 function App() {
-
   return (
     <div>
       <BrowserRouter>
         <Routes>
           <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
         </Routes>
       </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
